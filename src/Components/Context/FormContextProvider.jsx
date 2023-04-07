9122c4bd7a8dafa75bcf03b086b9776ce1aaf8d3
Avik Roy <somavik99@gmail.com>
2023-04-07 08:58:07 +0530

    New Components are arranged

--- src/Components/Context/FormContextProvider.jsx
+++ src/Components/Context/FormContextProvider.jsx
@@ -1,10 +1,25 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
 const initialPage = { Page: 0 };
 const FormContext = createContext(initialPage);
 
+let FormReducer = (state,action)=>{
+switch(action.type){
+  case "NEXT": return {...state, Page:state.Page+1 };
+  case "PREV": 
+    if("NEXT"){
+    return{...state, Page:state.Page-1};
+  }else{
+    return Page;
+  }
+default:
+  return;  
+}
+}
+
 const FormContextProvider = ({ children }) => {
-  return <FormContext.Provider>{children}</FormContext.Provider>;
+  const [state,dispatch] = useReducer(FormReducer,initialPage);
+  return <FormContext.Provider value={{state,dispatch}}>{children}</FormContext.Provider>;
 };
 
 export { FormContext, FormContextProvider };
--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -1,47 +1,66 @@
-import React, { useContext, useState } from "react";
-import {
-  ArrowLeftIcon,
-  ArrowRightIcon,
-} from "@heroicons/react/24/outline";
+import React, { useContext } from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import { FormContext } from "../Context/FormContextProvider";
 
-
 const Form = () => {
-  const {Page} = useContext(FormContext)
-  const [FormPage, setFormPage] = useState(0);
+  const { Page, state, dispatch } = useContext(FormContext);
+
   const FormHeader = [
     "Sign Up",
     "Personal Details",
     "Address",
     "Terms & conditions",
+    "Obligations",
   ];
-  const NextPage = () => {};
+  const NextPage = () => dispatch({ type: "NEXT" });
+  const PrevPage = () => dispatch({ type: "PREV" });
   return (
     <div className="Form">
       <div className="Form_container">
         <div className="header">
-          <h1>{FormHeader[FormPage]}</h1>
+          <h1>{FormHeader[state.Page]}</h1>
         </div>
-        <div className="body">
-        <div><input type="text" /></div>  
-        <div><input type="email" /></div>  
-        <div><input type="password" /></div>  
+        <div className="body m-5 p-5">
+          <div>
+            <input
+              type="text"
+              className="outline-0 border-l-[5px]  border-r-[5px]  border-t-2 border-b-2 m-2 p-2 rounded-lg"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              className="outline-0 border-l-[5px]  border-r-[5px]  border-t-2 border-b-2 m-2 p-2 rounded-lg "
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              className="outline-0 border-l-[5px]  border-r-[5px] border-t-2 border-b-2 m-2 p-2 rounded-lg"
+            />
+          </div>
         </div>
         <div className="footer">
-          <button className="w-2 h-2">
+          <button
+            disabled={state.Page == 0}
+            className="w-2 h-2"
+            onClick={PrevPage}
+          >
             <ArrowLeftIcon className="w-5 h-5" />
             Back
           </button>
         </div>
         <div>
-          <button onClick={NextPage}>
+          <button
+            disabled={state.Page == FormHeader.length - 1}
+            onClick={NextPage}
+          >
             <ArrowRightIcon className="w-5 h-5" />
             Next
           </button>
         </div>
       </div>
     </div>
-
   );
 };
 
